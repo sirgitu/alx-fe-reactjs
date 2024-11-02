dd1e34c6958d9745467493663c9ccb0b1c6fd8a0
Alvin Gitu <alvin.k.gitu@gmail.com>
2024-11-02 08:09:29 +0300

    react app creation

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -2,7 +2,7 @@ function WelcomeMessage() {
     return (
         <div>
             <h1>Hello everyone, I am learning React at ALX!</h1>
-            <P>I am learning about JSX</P>
+            <p>I am learning about JSX!</p>
             <p>This is a simple JSX component.</p>
         </div>
     );
