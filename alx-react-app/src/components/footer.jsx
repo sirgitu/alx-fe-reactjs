da6cbf3c41822c57149f3dc6ddb8dda8dabf23f9
Alvin Gitu <alvin.k.gitu@gmail.com>
2024-11-03 22:50:16 +0300

    made some chages

--- alx-react-app/src/components/footer.jsx
+++ alx-react-app/src/components/footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Footer() {
-    return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
 }
 
 export default Footer;
