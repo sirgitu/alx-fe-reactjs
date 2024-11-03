33dd0e0828afb119636392ea8c824fc65b07a6e1
Alvin Gitu <alvin.k.gitu@gmail.com>
2024-11-03 22:19:39 +0300

    made some chages

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
 function UserProfile({ name, age, bio }) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>Age: {age}</p>
-      <p>{bio}</p>
-    </div>
-  );
-}
-
-export default UserProfile;
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>Age: {age}</p>
+        <p>{bio}</p>
+      </div>
+    );
+  }
